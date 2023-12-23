390b1b624b80d28b530c6cd18bf023c90825d7c2
Yue-love <105182124+Yue-love@users.noreply.github.com>
2023-12-23 21:39:36 +0800

    Create profile_V1_SM6150T3.bat

---
+++ H220/profile_V1_SM6150T3.bat
@@ -0,0 +1,22 @@
+@ECHO OFF
+set main_docs=https://kdocs.cn/l/cune7WAK6oZX
+set block_docs=https://kdocs.cn/l/cune7WAK6oZX
+set link=https://yuelove.lanzouj.com/
+set lzdl=https://down.eebbk.top/bin/lzdl.7z
+set bff=%link%i72pU18ooe1i
+set magisk=%link%iJyrv15wcq8f
+set elf=%link%ixoNO13r8mda
+set toolbox=%link%iX1Fa17qt5sh
+set driver=%link%iEPJm15796ti
+set update_log=%link%iCYLV18ofihe
+set logo=%link%iK54c1anp6ub
+set key_bff=T3TE
+set key_magisk=T3TE
+set key_elf=T3TE
+set key_driver=1234
+set magisk_vercode=25200
+set checksum_bff=C892F69C
+set checksum_magisk=3A965DEE
+set checksum_elf=1285D92E
+set A=attrib +s +h /s /d
+set B=attrib -s -h /s /d
